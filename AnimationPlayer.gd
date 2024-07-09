a37d828a6e8e72371b8076fb0fc4d8b6f449a420
Hashir Shazad <136901596+HashirShazad@users.noreply.github.com>
2024-07-09 15:50:29 +0500

    Removee all files for test

--- AnimationPlayer.gd
+++
@@ -1,8 +0,0 @@
-extends AnimationPlayer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	play("idle")
-
-
--- Icon.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-
-
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("kill"):
-		self.queue_free()
-	if area.is_in_group("pkill"):
-		get_tree().reload_current_scene.bind().call_deferred()
